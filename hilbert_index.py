2e2331f71210fc7f06f40c1e9fbb6c1dd07e893d
twkang-spmdl <kangtw97@skku.edu>
2023-09-15 11:09:59 +0900

    .

--- hilbert_index.py
+++ hilbert_index.py
@@ -5,10 +5,10 @@ from hilbertcurve.hilbertcurve import HilbertCurve
 
 def generate_lattice(m, d):
     # The side length of the lattice along each dimension
-    side_length = 2 ** m
+    side_length = 2**m
     
     # Number of points in the lattice
-    num_points = side_length ** d
+    num_points = side_length**d
     
     # Initialize output array
     output = np.zeros((num_points, d))
@@ -22,32 +22,30 @@ def generate_lattice(m, d):
     return output
 
 # Lattice size is 2^m
-m = 3
+m = 5
 # Dimension of the system
 d = 2
-points = generate_lattice(m,d)
+points = generate_lattice(m, d)
+print(len(points))
 # the number of iterations to construc Hilbert curves
-p = round(math.pow(points.shape[0], 1/d))
+# p = round(math.pow(points.shape[0], 1/d))
+p = m
 # Considering the n-dimensional hypercube of side length 2^p
 num_points = (2**m)**d
 hilbert_curve = HilbertCurve(p, d)
 scan_idx = hilbert_curve.distances_from_points(points, match_type=False)
-# print(scan_idx)
-
-# print(type(scan_idx))
 
 # Scan idx for MATLAB
 mat_scan_idx = [i+1 for i in scan_idx]
 mat_scan_idx = np.array(mat_scan_idx)
-print(mat_scan_idx.reshape(d**m, d**m))
+
+# print(mat_scan_idx)
+
+def reshape_array(arr, d, m):
+    new_shape = [2**m]*d
+    return arr.reshape(new_shape)
+
+indMat = reshape_array(mat_scan_idx, d, m)
 
 from scipy.io import savemat
-savemat('indMat.mat', {'indMat': mat_scan_idx})
-
-test_array = np.zeros((5, 5))
-savemat('zeromat.mat', {'zeromat': test_array})
-
-# Sort original position using hilbert indices(distances)
-sort_idx = np.argsort(scan_idx)
-position_sorted = points[sort_idx]
-# print(position_sorted)
+savemat('indMat_2.mat', {'indMat_2': indMat})
